8077768d20a2850f9f6fad911213605fbe881b24
samaypanwar <69972858+samaypanwar@users.noreply.github.com>
2021-10-21 13:35:28 +0800

    UPD: init_exchange

--- exchange/init_exchange.py
+++ exchange/init_exchange.py
@@ -1,20 +1,26 @@
 from socket import *
-from socket import error as socket_error
+from socket import error as SOCKET_ERROR
 from utils.hyperparameters import *
 from data import *
-from typing import Any, BinaryIO, Dict
+from typing import BinaryIO
 from utils.data_types import *
-import json
 import time
+from communicate import read_from_exchange, write_to_exchange
 
 ########################
 ## EXCHANGE FUNCTIONS ##
 ########################
 
 def create_exchange() -> BinaryIO:
+    """Creates an exchange by making a socket and a makefile,
+    returns a binary buffer object related to the socket
+    """
+    # What is the current status of our server
     global SERVER_STATUS
+    # Initiating a socket as a unique item from a family
     sock = socket(AF_INET, SOCK_STREAM)
     print("Connecting to the server now...")
+    # Establish a connection with the server with the given port and IP
     sock.connect((EXCHANGE_HOSTNAME, PORT))
     print("Connected.")
     SERVER_STATUS = 1
@@ -22,42 +28,64 @@ def create_exchange() -> BinaryIO:
     return sock.makefile("rw", 1)
 
 def recreate_exchange() -> BinaryIO:
+    """[summary]
+    """
+
+    # Checking the status of the server and the current order id
     global SERVER_STATUS
     global ORDER_ID
     print("Reconnecting to server now...")
+
+    # Making a counter for the attempts for reconnection
     attempts = 0
+    # While not connected to server and you have tried less than 20 times
     while SERVER_STATUS == 0 and attempts < 20:
         try:
             attempts += 1
+            # Try creating an exchange
             exchange = create_exchange()
             SERVER_STATUS = 1
+            # Test the connection by sending a HELLO - command from the hyperparameters.py file
             write_to_exchange(exchange, HELLO)
+            # Read the response from the server
             response = read_from_exchange(exchange)
+            # IT is probably a JSON object as a dictionary
             print(response)
+            # If the type of response is 'hello'
             if response["type"] == str(InfoType.HELLO):
+                # Say that we are connected successfully
                 SERVER_STATUS = 1
+                # The response 'symbols' key contains each symbol and your position in that symbol
                 print("POSITIONS: {}".format(response["symbols"]))
-                symbols = response["symbols"]
-                for symbol in symbols:
-                    symbol_positions[symbol["symbol"]] = symbol["position"]
+                # Then to record our current position, add your current position to our symbol position dictionary
+                # which records all our current positions
+                all_symbols = response["symbols"]
+                for ticker in all_symbols:
+                    current_positions_in_symbols[ticker["symbol"]] = ticker["position"]
                 print("Reconnected!")
                 ORDER_ID = 0
+
+                # Return the binary buffer object that is created from create_exchange()
                 return exchange
+
+            # However, if the response of the server is that the market is 'open'
             elif response["type"] == str(InfoType.OPEN):
+
+                # Mark the symbols which are open as True in our symbol_open dictioanry
                 print("OPENING: {}".format(response["symbols"]))
                 for symbol in response["symbols"]:
-                    symbol_open[symbol] = True
+                    currently_open_symbols[symbol] = True
+
+                # Return the binary buffer object that is created from create_exchange()
                 return exchange
+
             else:
+                # If the response is neither 'open' nor 'hello' from the server, sleep and try again and
+                # set server status to 0
                 time.sleep(0.1)
                 SERVER_STATUS = 0
-        except socket_error:
-            print("Failed to reconnect, trying again...")
+
+        # If the socket returns an error, try again as well
+        except SOCKET_ERROR:
+            print(f"Attempt {attempts}: Failed to reconnect, trying again...")
             time.sleep(0.1)
-
-def write_to_exchange(exchange: BinaryIO, obj: Any) -> None:
-    json.dump(obj, exchange)
-    exchange.write("\n")
-
-def read_from_exchange(exchange: BinaryIO) -> Any:
-    return json.loads(exchange.readline())
