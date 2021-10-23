b7f2732419a252a4840a8bdc580a5931051c8620
samaypanwar <69972858+samaypanwar@users.noreply.github.com>
2021-10-23 11:20:14 +0800

    Update communicate.py

--- exchange/communicate.py
+++ exchange/communicate.py
@@ -2,7 +2,10 @@ import json
 from typing import BinaryIO, Any
 
 def read_from_exchange(exchange: BinaryIO):
-    return json.loads(exchange.readline())
+    line = exchange.readline()
+    print(type(line))
+    print(line)
+    return json.loads(line)
 
 def write_to_exchange(exchange: BinaryIO, obj: Any) -> None:
     json.dump(obj, exchange)
