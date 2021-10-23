423acb019df755fce8c2493e5a671f2a35042a27
samaypanwar <69972858+samaypanwar@users.noreply.github.com>
2021-10-23 11:03:20 +0800

    UPD

--- utils/hyperparameters.py
+++ utils/hyperparameters.py
@@ -11,7 +11,8 @@ PORT = 25000 + (TEST_EXCHANGE_INDEX if ENV == TEST_ENV else 0)
 ZEROETH_HOSTNAME = "0-prod-like"
 FIRST_HOSTNAME = "1-slower"
 SECOND_HOSTNAME = "2-empty"
-EXCHANGE_HOSTNAME = "test-exch-" + TEAM_NAME.lower() + " " + str(PORT) if(ENV==TEST_ENV) else PROD_ENV
+# EXCHANGE_HOSTNAME = "test-exch-" + TEAM_NAME.lower() + " " + str(PORT) if(ENV==TEST_ENV) else PROD_ENV
+EXCHANGE_HOSTNAME = '10.0.251.221'
 
 HELLO = { "type": "hello", "team": TEAM_NAME.upper() }
 
